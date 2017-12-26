48adf4c7e27e00ff0b4b97747f15cd27e7da55ed
= <precogtyrant@gmail.com>
2017-12-26 13:57:48 +0545

    top comments

--- titanic/train.py
+++ titanic/train.py
@@ -1,3 +1,9 @@
+#Predicting survivors on the titanic dataset using Logistic Regression module of SciKit Learn
+#Prediction accuracy score consistently between 80% to 84%, by randomly splitting the 
+#data into train/test sets using sklearn's train_test_split module on a 80/20 basis
+#accuracy is 85% when taking the last 20% of the data as the testing data and the remaining 
+#as the training data
+
 # %matplotlib inline
 import matplotlib.pyplot as plt
 import math
@@ -15,13 +21,6 @@ def get_title(name):
         title = "Mr."
     title
 
-def rounder(num):
-    """rounding off logic"""
-    # if num <= 0.5:
-    #     int(math.floor(num))
-    # else:
-    #     int(round(num))
-    int(round(num))
 
 
 titanic_data = pd.read_csv("titanic.csv")
@@ -33,6 +32,8 @@ titanic_data["Sex"] = pd.get_dummies(titanic_data["Sex"])
 titanic_data["Age"].fillna(titanic_data["Age"].mean(), inplace = True)
 titanic_data["Pclass"] = pd.get_dummies(titanic_data["Pclass"])
 
+#using the first letter of the cabin as the cabin identifier and setting the one with the highest frequency
+#as the cabin class for those rows where its missing(it was missing in over 600 rows)
 titani_cabin_mode = titanic_data["Cabin"].dropna().apply(lambda x: x[0]).mode() #cabin with highest frequency
 titanic_data["Cabin"] = titanic_data["Cabin"].dropna().apply(lambda x: x[0]) #dropna so that function can be applied without error
 titanic_data["Cabin"].fillna(titani_cabin_mode[0],inplace=True)
@@ -42,7 +43,7 @@ titanic_embarked_mode = titanic_data["Embarked"].dropna().mode()
 titanic_data["Embarked"].fillna(titanic_embarked_mode,inplace=True)
 titanic_data["Embarked"] = pd.get_dummies(titanic_data["Embarked"])
 
-#train,test = titanic_data[:-20],titanic_data[-20:]
+#train,test = titanic_data[:-20],titanic_data[-20:] #80/20 split manually
 train,test = train_test_split(titanic_data,test_size = 0.2)
 
 model = LogisticRegression();
@@ -52,5 +53,5 @@ train_Y = train["Survived"]
 model.fit(train_x,train_Y)
 prediction = model.predict(test_x)
 prediction_result = pd.DataFrame({"Survived":prediction,"PassengerId":test["PassengerId"], "Name":test["Name"]})
-prediction_result["Survived"] = prediction_result["Survived"].apply(lambda x: rounder(x))
+prediction_result["Survived"] = prediction_result["Survived"]
 print(accuracy_score(test["Survived"],prediction.astype(int)))
